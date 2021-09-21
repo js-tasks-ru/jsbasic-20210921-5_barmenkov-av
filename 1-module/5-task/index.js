31e524d5ba4d11281853ac735966f21917c0c819
Andrei Barmenkov <barmenkov.av@yandex.ru>
2021-09-21 21:36:26 +0300

    module-1-task-5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,3 @@
 function truncate(str, maxlength) {
-  return (str.length < maxlength)?str:(str.slice(0,maxlength-1) + '…');
+  return (str.length < maxlength) ? str : (str.slice(0, maxlength - 1) + '…');
 }
